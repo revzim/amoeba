e780d84033487e8c99f1b1cb22a0736904600f43
= <7407916+revzim@users.noreply.github.com>
2021-10-18 21:28:19 -0400

    0.0.15

--- crypt/crc.go
+++ crypt/crc.go
@@ -9,7 +9,7 @@ import (
 type (
 
 	// CRCGenerator --
-	// (PSEUDO-RANDOM REPLICATABLE SEEDED GENERATION)
+	// (PSEUDO-RANDOM REPLICABLE SEEDED GENERATION)
 	CRCGenerator struct {
 		*rand.Rand
 		poly          uint64
